bb6714cd870031fc2186ed9d559b70fd8b1f88c4
Tim Miller <broadcaststorm@users.noreply.github.com>
2018-12-05 21:55:56 -0500

    Solution to Day 1, puzzles 1 and 2.

---
+++ 2018/01/part1.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+"""
+Part 1:  Determine the final frequency
+
+Each line of the file contains the shift in frequency (v).
+So, start at 0, loop over each line of the file, add the
+shift to the current frequency.
+"""
+
+if __name__ == '__main__':
+    v = 0
+    with open('input.txt', 'r') as f:
+        for l in f:
+            v = v + int(l.rstrip())
+    print('Frequency shift is {}'.format(v))
---
+++ 2018/01/part2.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+"""
+Part 2: Find the first repeated frequency value
+
+Loop over the input file (multiple times if need be),
+process the frequency shifts like before but, this time, we
+need to track each frequency encountered to find when we repeat
+a particular frequency.
+
+A "brute force" way to do this is simply store each value in a list
+and search the entire list for a repeated value.  Since the search
+is O(N), that will progressively get slower and slower which each
+shift and consume progressively more memory.
+
+My general idea - Rather than store every possible number let's create
+essentially a character-based bit array.  Because I'm lazy, one for
+negative and one for positive.
+
+Also, because I don't know the maximum frequency value, provide some
+logic to expand (and, lazy again, restart the process from scratch)
+
+"""
+
+import sys
+
+
+def get_arrays(length=1000000):
+    return '0'*length, '0'*length
+
+
+if __name__ == '__main__':
+    v = 0
+    positive, negative = get_arrays(100000)
+
+    while(True):
+        print('Beginning input with frequency {}'.format(v))
+
+        with open('input.txt', 'r') as f:
+            for l in f:
+                v = v + int(l.rstrip())
+
+                # If I'm going to overrun, resize and restart
+                if v > len(positive):
+                    print('Bigger array needed: restarting')
+                    new_length = len(positive)*10
+                    positive, negative = get_arrays(new_length)
+                    v = 0
+                    break
+
+                # Check to find previously found frequency
+                if (v > 0):
+                    check = positive
+                elif (v < 0):
+                    check = negative
+                else:
+                    print('Duplicate frequency is 0')
+                    sys.exit(0)
+
+                if int(check[v]) == 1:
+                    print('Found duplicate frequency: {}'.format(v))
+                    sys.exit(0)
+
+                if (v > 0):
+                    positive = positive[0:v] + '1' + positive[v+1:]
+                else:
+                    negative = negative[0:v] + '1' + negative[v+1:]
