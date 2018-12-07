98026ed4aaf07474318a799354ef3071117c4c55
Tim Miller <broadcaststorm@users.noreply.github.com>
2018-12-06 22:42:04 -0500

    Update Day 1 solution to explicitly require Python 3

--- 2018/01/part1.py
+++ 2018/01/part1.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 """
 Part 1:  Determine the final frequency
 
--- 2018/01/part2.py
+++ 2018/01/part2.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 """
 Part 2: Find the first repeated frequency value
 
