38952aaa3dc7d655c1da9f14ee23fd3f6ea17d34
Tim Miller <broadcaststorm@users.noreply.github.com>
2022-12-04 06:51:38 +0000

    add template directorry

---
+++ 2022/template/solution.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""
+2022/template/solution.py: Python based solution in template folder
+"""
+
+
+def part1(filename: str = 'input.txt'):
+    pass
+
+
+def part2(filename: str = 'input.txt'):
+    pass
+
+
+if __name__ == '__main__':
+
+    # Solve part1
+    result = part1(filename='input-part1.txt')
+    print(f'Result is {result}')
+
+    # Solve part2
+    output = part2(filename='input-part2.txt')
+    print(f'Output is {output}')
